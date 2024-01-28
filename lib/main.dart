4b05ebf3e2e8c1c62bb5c5fd75a262d6ac2c36e3
amalnaroth <amalzenox129@gmail.com>
2024-01-28 12:48:38 +0530

    home page complete

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:todo_sqflite/feature/todo_app/presentation/todo_list_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.purple,
+        ),
+        useMaterial3: true,
+      ),
+      home: const TodolistScreen(),
+    );
+  }
+}
